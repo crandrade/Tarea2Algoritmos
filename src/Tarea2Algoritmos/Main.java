048e4c47d786eecbdb473b307f5ebbbe5957b465
crandrade <crandrad@dcc.uchile.cl>
2015-10-27 16:28:35 -0300

    Agregados paquetes útiles y Main

---
+++ src/Tarea2Algoritmos/Main.java
@@ -0,0 +1,14 @@
+package Tarea2Algoritmos;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

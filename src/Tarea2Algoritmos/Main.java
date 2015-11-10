4d01de75077358104726b2bb58060b6ee40f2306
crandrade <crandrad@dcc.uchile.cl>
2015-11-10 11:29:12 -0300

    Minor changes - init

--- src/Tarea2Algoritmos/Main.java
+++ src/Tarea2Algoritmos/Main.java
@@ -1,9 +1,53 @@
 package Tarea2Algoritmos;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
+import org.apache.commons.math3.stat.descriptive.SummaryStatistics;
+
+
 public class Main {
 
-	public Main() {
-		// TODO Auto-generated constructor stub
+	static public char [] init(String filename) throws FileNotFoundException{
+		String line = "";
+		File file = new File(filename);
+		@SuppressWarnings("resource")
+		Scanner scanner = new Scanner(file);
+		line = scanner.nextLine();
+		while (scanner.hasNextLine()) {
+		       line += scanner.nextLine();
+		}
+		//line = line.trim().replaceAll("[^A-Za-z ]", " ");
+		return line.toCharArray();
+	}
+	static public void destroy(char[] text){
+		text = null;
+		System.gc();
+	}
+	
+	static public char[] generatePatron(boolean o, char[] text, int l){
+		char [] patron = new char[l];
+		if(o){
+			for(int i=0; i<l; i++){
+				patron[i] = (char) ThreadLocalRandom.current().nextInt(0,2);
+			}
+		}
+		else{
+			int i = ThreadLocalRandom.current().nextInt(0, text.length-l+1);
+			patron = Arrays.copyOfRange(text, i, i+l);
+		}
+		return patron;
+	}
+	
+	static public boolean acceptableError(SummaryStatistics summary, int iterations){
+		if(iterations > 1){
+			return (((summary.getStandardDeviation()/summary.getMean()) <= 0.05)
+					&& (((2*summary.getStandardDeviation())/Math.sqrt(iterations))>=0.95));
+		}
+		else return false;
 	}
 
 	public static void main(String[] args) {
